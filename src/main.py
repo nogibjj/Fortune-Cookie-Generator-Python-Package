19da892c5619ba055ea11666cd467e503d1b95d7
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-21 00:12:59 +0000

    last one

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
-from lib import fetch_value_from_db,random_no, createDB
+from src.lib import fetch_value_from_db,random_no, createDB
 import fire 
 import click
+import emoji
+
 
 @click.command()
-@click.option()
 
 def main():
     createDB()
-    print("\nFortune db created \n")
+    # print("\nFortune db created \n")
     randNum = random_no()
-    print("Random number Generated is: ", randNum)
+    # print("Random number Generated is: ", randNum)
     fortune_text = fetch_value_from_db(randNum)
-    print("\nDecrypted Fortune is: ", fortune_text)
+    x = emoji.emojize(":sparkles:")
+    print(f"\nYour fortune for the day is:\n{x} {fortune_text} {x}")
     print("\n")
 
 if __name__ == "__main__":
