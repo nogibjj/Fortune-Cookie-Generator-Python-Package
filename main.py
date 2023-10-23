729039be4de4b062f9c6ca96989b55504b342b43
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-10-23 23:01:43 +0000

    import error resolved

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import click
 import emoji
 # from .lib import fetch_value_from_db,random_no, createDB
 # from .data import fortune_data_values
-from .lib import fetch_value_from_db,random_no, createDB
-from .data import fortune_data_values
+from .src.lib import fetch_value_from_db,random_no, createDB
+from .src.data import fortune_data_values
 
 # try:
 #     import lib
